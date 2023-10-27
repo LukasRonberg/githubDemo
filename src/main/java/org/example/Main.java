235d3850414f10c59e3018aff2474fc52b5c07de
LukasRonberg <143714608+LukasRonberg@users.noreply.github.com>
2023-10-27 12:08:37 +0200

    Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        int val = 1;
+
+        if(val == 1){
+            System.out.println("The value is equal to 1");
+        } else {
+            System.out.println("The value is not equal to 1");
+        }
     }
 }
